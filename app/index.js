f8ab7a6e501b3e777c1be82a64b4a6256a594c1b
harikiranmp <95916935+harikiranmp@users.noreply.github.com>
2025-10-23 21:12:05 +0530

    Update index.js

--- app/index.js
+++ app/index.js
@@ -2,6 +2,6 @@
 const http = require('http');
 const PORT = 3000;
 const server = http.createServer((req, res) => {
-  res.end(`Hey Dinesh, "HAPPY DIWALI" and running Time: ${new Date()}\n`);
+  res.end(`Sample app is running inside private network and running Time: ${new Date()}\n`);
 });
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
